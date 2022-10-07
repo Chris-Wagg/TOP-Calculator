0576d2fb3a58a8bb2f98ae13dfce3b8cf90b4e0d
Chris-Wagg <cgwagg@hotmail.co.nz>
2022-10-07 11:23:16 +0200

    html button changes and data attributes added

--- index.js
+++ index.js
@@ -1,61 +1,65 @@
-console.log('hello world')
+// let display = document.getElementById('display')
 
-let currentNum = ''
-let previousNum = ''
-let operator = ''
+// let buttons = Array.from(document.getElementsByClassName('button'))
 
-function add(numOne, numTwo) {
-	let total = numOne + numTwo
-	console.log(total)
-}
-function subtract(numOne, numTwo) {
-	let total = numOne - numTwo
-	console.log(total)
-}
-function multiply(numOne, numTwo) {
-	let total = numOne * numTwo
-	console.log(total)
-}
-function divide(numOne, numTwo) {
-	let total = numOne / numTwo
-	console.log(total)
-}
+// buttons.map((button) => {
+// 	button.addEventListener('click', (e) => {
+// 		switch (e.target.innerText) {
+// 			case 'C':
+// 				display.innerText = ''
+// 				break
 
-function operate(string) {
-	const valOne = string.split
-	const operator = string.slice(string.indexOf('+'))
-	// string.includes('+') ||
-	// string.includes('-') ||
-	// string.includes('/') ||
-	// string.includes('x')
-	const valTwo = string.split
-	console.log(operator)
+// 			case '=':
+// 				test(display.innerText)
+// 				break
 
-	if (operator === '+') {
-		add(previousNum, currentNum)
-	} else if (operator === '-') {
-		subtract(previousNum, currentNum)
-	} else if (operator === '*') {
-		multiply(previousNum, currentNum)
-	} else if (operator === '/') {
-		divide(previousNum, currentNum)
-	}
-}
-let display = document.getElementById('display')
+// 			default:
+// 				display.innerText += e.target.innerText
+// 		}
+// 	})
+// })
 
-let buttons = Array.from(document.getElementsByClassName('button'))
+// // function setValAndPlus() {
+// // 	let value = display.innerText
+// // 	display.innerText = ''
+// // 	return value
+// // }
 
-buttons.map((button) => {
-	button.addEventListener('click', (e) => {
-		switch (e.target.innerText) {
-			case 'C':
-				display.innerText = ''
-				break
-			case '=':
-				operate(display.innerText)
-				break
-			default:
-				display.innerText += e.target.innerText
-		}
-	})
-})
+// function operate(string) {
+// 	let operatorIndex = string.indexOf('+', '=', '-', 'x')
+// 	console.log(operatorIndex)
+// 	let valTwo = string.slice(operatorIndex + 1)
+// 	// console.log(valTwo)
+// 	let valOne = string.slice(0, string.indexOf(operatorIndex - operatorIndex))
+// 	// console.log(valOne)
+
+// 	let operator = ''
+// 	if (string.includes('+')) {
+// 		operator = '+'
+// 	} else if (string.includes('-')) {
+// 		operator = '-'
+// 	} else if (string.includes('/')) {
+// 		operator = '/'
+// 	} else if (string.includes('x')) {
+// 		operator = 'x'
+// 	}
+
+// 	let finalValue = 0
+
+// 	if (operator === '+') {
+// 		finalValue = valOne + valTwo
+// 	} else if (operator === '-') {
+// 		finalValue = valOne - valTwo
+// 	} else if (operator === '*') {
+// 		finalValue = valOne * valTwo
+// 	} else if (operator === '/') {
+// 		finalValue = valOne / valTwo
+// 	}
+// }
+
+// function test(innerText) {
+// 	let input = innerText
+// 	let result = Function('return ' + input)()
+
+// 	console.log(result)
+// }
